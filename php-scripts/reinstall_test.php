4e01b87f02096cb54429a52d5678b6778cd08181
dlb213 <dlb213@lehigh.edu>
2021-08-30 17:16:52 -0400

    new script for -parallell- execution of testing this script. made serious progress. But for some reason deleting nodes is giving me problems.

--- php-scripts/reinstall_test.php
+++ php-scripts/reinstall_test.php
@@ -4,39 +4,46 @@
 
 use Drupal\node\Entity\Node;
 use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\file\Entity\File;
+use Drupal\media\Entity\Media;
 use Drupal\menu_link_content\Entity\MenuLinkContent;
 
 // Load All nodes.
-echo("loading nodes");
 $node_result = \Drupal::entityQuery('node')->execute();
 $nodes_storage = \Drupal::entityTypeManager()->getStorage('node');
 $node_multiple = $nodes_storage->loadMultiple($node_result);
 
-echo("Loading paragraphs");
+$file_result = \Drupal::entityQuery('file')->execute();
+$files_storage = \Drupal::entityTypeManager()->getStorage('file');
+$file_multiple = $files_storage->loadMultiple($file_result);
+
+$media_result = \Drupal::entityQuery('media')->execute();
+$media_storage = \Drupal::entityTypeManager()->getStorage('media');
+$media_multiple = $media_storage->loadMultiple($media_result);
+
 $par_result = \Drupal::entityQuery('paragraph')->execute();
 $par_storage = \Drupal::entityTypeManager()->getStorage('paragraph');
 $par_multiple = $par_storage->loadMultiple($par_result);
 
-echo("loading menus");
-$menu_result = \Drupal::entityQuery('menu_link_content')->execute();
+$menu_result = \Drupal::entityQuery('menu_link_content')
+->condition('bundle', ['social-media'],'NOT IN')
+->execute();
 $menu_storage = \Drupal::entityTypeManager()->getStorage('menu_link_content');
 $menu_multiple = $menu_storage->loadMultiple($menu_result);
 
 /*
-$first_nid = array_keys($node_multiple)[0];
-print_r($first_nid);
-echo("\n");
-print_r($node_multiple[$first_nid]->toArray());
+$menu_result = \Drupal::entityQuery('menu_link_content')->execute();
+$menu_storage = \Drupal::entityTypeManager()->getStorage('menu_link_content');
+$menu_multiple = $menu_storage->loadMultiple($menu_result);
 */
+
+foreach($menu_multiple as $menuid => $menuobj){
+     echo("does this work?");
+     print_r($menuobj->toArray());
+}
+
 
 //probably need to create one array for each entity type so that it can be loaded individually.
-
-/*1. load all entities, save them to an array or several arrays
-     you have to retain the node ids
-     1.a. 
-2. run the reinstallation script
-3. reload the entities into the site.
-*/
 
 
 $myoutput = [];
@@ -47,50 +54,54 @@ foreach($myoutput as $output) {
 }
 
 #delete nodes that already from the new profile.
-echo("deleting nodes from new site");
 $new_node_result = \Drupal::entityQuery('node')->execute();
-//$new_nodes_storage = \Drupal::entityTypeManager()->getStorage('node');
-//$new_node_multiple = $new_nodes_storage->loadMultiple($new_node_result);
-$multipleloaded = Node::loadMultiple($new_node_result);
-foreach($multipleloaded as $delnid => $delete_node) {
-     print_r($delete_node);
+$new_nodes_storage = \Drupal::entityTypeManager()->getStorage('node');
+$new_node_multiple = $new_nodes_storage->loadMultiple($new_node_result);
+foreach($new_node_multiple as $delete_node) {
      $delete_node->delete();
 }
 
+#files
+foreach($file_multiple as $file => $one_file) {
+     //print_r($one_file->toArray());
+     $newfile = File::create($one_file->toArray());
+     $newfile->save();
+}
+
+#media
+foreach($media_multiple as $media => $one_media) {
+     //print_r($one_file->toArray());
+     $newmedia = Media::create($one_media->toArray());
+     $newmedia->save();
+}
+
 #paragraphs
-echo("recreating paragraphs");
 foreach($par_multiple as $pid => $one_par) {
-     print_r($one_par->toArray());
+     //print_r($one_par->toArray());
      $newpar = Paragraph::create($one_par->toArray());
      $newpar->save();
 }
 
 #node 
-echo("recreating nodes");
 foreach($node_multiple as $nid => $one_node) {
      //print_r($one_node->toArray());
      $newnode = Node::create($one_node->toArray());
      $newnode->save();
 } 
 
-/*
 #menu
-echo("recreating menus");
 foreach($menu_multiple as $menu => $one_menu) {
-     print_r($one_menu->toArray());
+     //print_r($one_node->toArray());
      $newmenu = MenuLinkContent::create($one_menu->toArray());
      $newmenu->save();
 }
-*/
-
-//MenuLinkContent::create(
 
 
 /* # entities:
 (roughly in order)
 file
 media
-paragraphs
+X paragraphs
 X nodes
 menu_links
 block_content
